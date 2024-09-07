35a44cdfe300d0ed573554cbdf7483263eb44a23
Alejandro Fajardo <jorge.fajardo@globant.com>
2024-09-07 13:56:18 -0500

    updated Test method name to verifySuccessfullyLogOut

--- src/test/java/com/globant/tests/LogoutTest.java
+++ src/test/java/com/globant/tests/LogoutTest.java
@@ -14,17 +14,17 @@ public class LogoutTest extends BaseTest {
     public static final Logger log = LoggerFactory.getLogger(LogoutTest.class);
 
     @Test(dataProvider = "persona", dataProviderClass = DataProviders.class)
-    public void verifyPurchaseSuccessForOneItem(String name, String lastName, String postalCode) {
+    public void verifySuccessfullyLogOut(String name, String lastName, String postalCode) {
         log.info("Login into the home page");
         HomePage homePage = loginPage.returnHomeScreen(getDriver());
         log.info("Verifying that the user is in the home page");
-        assertTrue(homePage.isElementVisible(homePage.getBtnReactBurgerMenu(),4));
-        assertTrue(homePage.isElementVisible(homePage.getAddToCartButtons().get(0),3));
+        assertTrue(homePage.isElementVisible(homePage.getBtnReactBurgerMenu(), 4));
+        assertTrue(homePage.isElementVisible(homePage.getAddToCartButtons().get(0), 3));
         homePage.clickElement(homePage.getBtnReactBurgerMenu());
-        log.info("Logout button is clickable:{}",homePage.isElementVisible(homePage.getBtnLogout(),2));
+        log.info("Logout button is clickable:{}", homePage.isElementVisible(homePage.getBtnLogout(), 2));
         homePage.clickElement(homePage.getBtnLogout());
         log.info("Verifying log out");
-        homePage.isElementVisible(loginPage.getLoginBtn(),3);
+        homePage.isElementVisible(loginPage.getLoginBtn(), 3);
         log.info("Log out successful");
     }
 }
